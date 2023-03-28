cce20918d0db5083af9d382eee2d74ebc196cf7f
arifremal <arifremal013@gmail.com>
2023-03-28 15:30:07 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+      <img src={logo} alt="" />
+      <div className='navstyle'>
+      <a href="/Shop">Shop</a>
+      <a href="/order">order</a>
+      <a href="/Inventory">inventory</a>
+      <a href="/Login">login</a>
+      </div>
+
+        </nav>
+    );
+};
+
+export default Header;
